cb235e029717acbd5dbdfbb1ca2b9d9793dea0d6
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2023-01-26 14:17:14 +0900

    :bento: dot-ui-kit SVG asset 추가

---
+++ libs/dot-ui-kit/src/assets/icons/index.ts
@@ -0,0 +1,33 @@
+import ADDRESS from './address.svg?component';
+import CLOSE from './close.svg?component';
+import EDIT from './edit.svg?component';
+import EMAIL from './eMail.svg?component';
+import HEART from './heart.svg?component';
+import KEY from './key.svg?component';
+import LOCK from './lock.svg?component';
+import MENU from './menu.svg?component';
+import MOON from './moon.svg?component';
+import NEXT from './next.svg?component';
+import PERSON from './person.svg?component';
+import PREV from './prev.svg?component';
+import SEARCH from './search.svg?component';
+import SHOPPING_CART from './shopping_cart.svg?component';
+import SUN from './sun.svg?component';
+
+export {
+  MENU,
+  EDIT,
+  PERSON,
+  SHOPPING_CART,
+  HEART,
+  SEARCH,
+  MOON,
+  SUN,
+  NEXT,
+  PREV,
+  LOCK,
+  CLOSE,
+  ADDRESS,
+  EMAIL,
+  KEY,
+};
